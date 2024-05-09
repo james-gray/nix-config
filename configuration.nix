c40b871ea0cd685dcbf9869f9c921566738ebe8f
James Gray <james.matthew.gray@gmail.com>
2024-05-08 18:10:56 -0700

    Disable NetworkManager-wait-online
    Not sure why, but it fails to start. Probably look into this later.

--- configuration.nix
+++ configuration.nix
@@ -538,6 +538,14 @@
     };
   };
 
+  systemd = {
+    services = {
+      "NetworkManager-wait-online" = {
+        enable = false;
+      };
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
