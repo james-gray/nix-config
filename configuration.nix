0d003c07a7012c882b7fe2e9e47f3b980b527dae
James Gray <james.matthew.gray@gmail.com>
2024-08-18 11:01:09 -0700

    Migrate Home Assistant AND nginx reverse proxy config to hal9000
    I broked nginx catastrophically, somehow, on my raspberry pi. Since it's gross and horrible over there with no version control, I've (mostly) nixified it (with the exception of Nextcloud and Jellyfin, which I couldn't get working without using extraConfig. Eventually I'll try to fix those too

--- configuration.nix
+++ configuration.nix
@@ -32,6 +32,7 @@
       ffmpeg
       lm_sensors
       netdata
+      nginx
       rclone
       sanoid
       smartmontools
@@ -90,6 +91,161 @@
         };
         ml = { "enabled" = "no"; };
       };
+    };
+
+    nginx = {
+      enable = true;
+      sslDhparam = "/tank9000/ds1/nginx/certs/dhparams.pem";
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+      virtualHosts = let
+        SSL = {
+          forceSSL = true;
+          sslCertificate = "/tank9000/ds1/nginx/certs/cert.pem";
+          sslCertificateKey = "/tank9000/ds1/nginx/certs/key.pem";
+        }; in {
+          "actual.jgray.me" = ( SSL // { locations."/".proxyPass = "http://127.0.0.1:5006/"; });
+          "hass.jgray.me" = ( SSL // {
+            locations = {
+              "/" = {
+                proxyPass = "http://127.0.0.1:8123/";
+                proxyWebsockets = true;
+                extraConfig = ''
+                  proxy_set_header Host $host;
+                  proxy_set_header X-Real-IP $remote_addr;
+                  proxy_set_header Upgrade $http_upgrade;
+                  proxy_set_header Connection "upgrade";
+                '';
+              };
+            };
+          });
+          "jellyfin.jgray.me" = ( SSL // {
+            extraConfig = ''
+              if ($scheme = "http") {
+                  return 301 https://$host$request_uri;
+              }
+
+              ## The default `client_max_body_size` is 1M, this might not be enough for some posters, etc.
+              client_max_body_size 20M;
+
+              ssl_session_timeout 1d;
+              ssl_session_cache shared:MozSSL:10m; # about 40000 sessions
+              ssl_session_tickets off;
+
+              ssl_protocols TLSv1.2 TLSv1.3;
+              ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305;
+              ssl_prefer_server_ciphers on;
+              set $jellyfin 127.0.0.1;
+
+              add_header Strict-Transport-Security "max-age=3153600" always;
+              #add_header X-Frame-Options "SAMEORIGIN";
+              #add_header X-XSS-Protection "0"; # Do NOT enable. This is obsolete/dangerous
+              #add_header X-Content-Type-Options "nosniff";
+
+              add_header Cross-Origin-Opener-Policy "same-origin" always;
+              add_header Cross-Origin-Embedder-Policy "require-corp" always;
+              add_header Cross-Origin-Resource-Policy "same-origin" always;
+
+              location / {
+                  proxy_pass http://$jellyfin:8096;
+
+                  proxy_set_header Host $host;
+                  proxy_set_header X-Real-IP $remote_addr;
+                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                  proxy_set_header X-Forwarded-Proto $scheme;
+                  proxy_set_header X-Forwarded-Protocol $scheme;
+                  proxy_set_header X-Forwarded-Host $http_host;
+
+                  # Disable buffering when the nginx proxy gets very resource heavy upon streaming
+                  proxy_buffering off;
+              }
+
+              location = /web/ {
+                  # Proxy main Jellyfin traffic
+                  proxy_pass http://$jellyfin:8096/web/index.html;
+                  proxy_set_header Host $host;
+                  proxy_set_header X-Real-IP $remote_addr;
+                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                  proxy_set_header X-Forwarded-Proto $scheme;
+                  proxy_set_header X-Forwarded-Protocol $scheme;
+                  proxy_set_header X-Forwarded-Host $http_host;
+              }
+
+              location /socket {
+                  # Proxy Jellyfin Websockets traffic
+                  proxy_pass http://$jellyfin:8096;
+                  proxy_http_version 1.1;
+                  proxy_set_header Upgrade $http_upgrade;
+                  proxy_set_header Connection "upgrade";
+                  proxy_set_header Host $host;
+                  proxy_set_header X-Real-IP $remote_addr;
+                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                  proxy_set_header X-Forwarded-Proto $scheme;
+                  proxy_set_header X-Forwarded-Protocol $scheme;
+                  proxy_set_header X-Forwarded-Host $http_host;
+              }
+            '';
+          });
+          "lubelogger.jgray.me" = ( SSL // { locations."/".proxyPass = "http://127.0.0.1:48080/"; });
+          "mealie.jgray.me" = ( SSL // { locations."/".proxyPass = "http://127.0.0.1:9925/"; });
+          "nextcloud.jgray.me" = ( SSL // {
+            extraConfig = ''
+              if ($scheme = "http") {
+                  return 301 https://$host$request_uri;
+              }
+
+              location / {
+                  proxy_pass http://127.0.0.1:11000$request_uri;
+
+                  proxy_buffering off;
+                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                  proxy_set_header X-Forwarded-Port $server_port;
+                  proxy_set_header X-Forwarded-Scheme $scheme;
+                  proxy_set_header X-Forwarded-Proto $scheme;
+                  proxy_set_header X-Real-IP $remote_addr;
+                  proxy_set_header Accept-Encoding "";
+                  proxy_set_header Host $host;
+
+                  client_body_buffer_size 512k;
+                  proxy_read_timeout 86400s;
+                  client_max_body_size 0;
+
+                  # Websocket
+                  proxy_http_version 1.1;
+                  proxy_set_header Upgrade $http_upgrade;
+                  proxy_set_header Connection $connection_upgrade;
+              }
+
+              # Make a regex exception for `/.well-known` so that clients can still
+              # access it despite the existence of the regex rule
+              # `location ~ /(\.|autotest|...)` which would otherwise handle requests
+              # for `/.well-known`.
+              location ^~ /.well-known {
+                  # The rules in this block are an adaptation of the rules
+                  # in `.htaccess` that concern `/.well-known`.
+
+                  location = /.well-known/carddav { return 301 /remote.php/dav/; }
+                  location = /.well-known/caldav  { return 301 /remote.php/dav/; }
+
+                  location /.well-known/acme-challenge    { try_files $uri $uri/ =404; }
+                  location /.well-known/pki-validation    { try_files $uri $uri/ =404; }
+
+                  # Let Nextcloud's API for `/.well-known` URIs handle all other
+                  # requests by passing them to the front-end controller.
+                  return 301 /index.php$request_uri;
+              }
+
+              ssl_session_timeout 1d;
+              ssl_session_cache shared:MozSSL:10m; # about 40000 sessions
+              ssl_session_tickets off;
+
+              ssl_protocols TLSv1.2 TLSv1.3;
+              ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305;
+              ssl_prefer_server_ciphers on;
+            '';
+          });
+          "vw.jgray.me" = ( SSL // { locations."/".proxyPass = "http://127.0.0.1:180/"; });
+        };
     };
 
     # Note that to enable macos mounts of ZFS datasets over NFS within a Tailscale tailnet, must set as follows (e.g. for dataset tank9000/example):
@@ -242,7 +398,7 @@
       enable = true;
       allowPing = true;
       trustedInterfaces = [ "tailscale0" ];
-      allowedTCPPorts = [ 22 2049 137 138 139 445 ];
+      allowedTCPPorts = [ 22 2049 137 138 139 445 80 443 ];
       allowedUDPPorts =
         [ 22 2049 137 138 139 445 config.services.tailscale.port ];
     };
@@ -311,6 +467,25 @@
           ExecStop = ''
             ${pkgs.docker-compose}/bin/docker-compose -f ${
               ./frigate/docker-compose.yml
+            } stop
+          '';
+          RemainAfterExit = true;
+        };
+        after = [ "docker.service" ];
+        requires = [ "docker.service" ];
+        wantedBy = [ "default.target" ];
+      };
+      homeassistant = {
+        enable = true;
+        serviceConfig = {
+          ExecStart = ''
+            ${pkgs.docker-compose}/bin/docker-compose -f ${
+              ./hass/docker-compose.yml
+            } up -d
+          '';
+          ExecStop = ''
+            ${pkgs.docker-compose}/bin/docker-compose -f ${
+              ./hass/docker-compose.yml
             } stop
           '';
           RemainAfterExit = true;
@@ -566,6 +741,12 @@
   users = {
     motd = "I'm sorry, Dave, I'm afraid I can't do that.";
     users = {
+      homeassistant = with pkgs; {
+        description = "home assistant user";
+        isSystemUser = true;
+        group = "homeassistant";
+        uid = 998;
+      };
       "james.gray" = with pkgs; {
         description = "James Gray (macos)";
         extraGroups = [ "macos" ];
@@ -593,6 +774,7 @@
       };
     };
     groups = {
+      homeassistant = { gid = 994; };
       syncoid = { };
       timemachine = { };
       "james.gray" = { };
